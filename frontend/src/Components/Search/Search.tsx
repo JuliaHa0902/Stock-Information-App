1cd13651a9dca0f2836017b742c328a26b4d2053
Julia Ha <juliaha0902@gmail.com>
2025-01-13 22:50:19 -0600

    Add finance api and basic search component

---
+++ frontend/src/Components/Search/Search.tsx
@@ -0,0 +1,22 @@
+import React, { ChangeEvent, useState, SyntheticEvent } from 'react'
+
+type Props = {}
+
+const Search : React.FC<Props> = (props: Props): JSX.Element => {
+    const [search, setSearch] = useState<string>("");
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+        console.log(e);
+    }
+    const onClick = (e: SyntheticEvent) => {
+        console.log("Click");
+    };
+    return (
+        <div>
+            <input value={search} onChange={(e) => handleChange(e)}></input>
+            <button onClick={(e) => onClick(e)}>Search</button>
+        </div>
+    )
+}
+
+export default Search
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { searchCompanies } from './api';
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+console.log(searchCompanies("tsla"));
 root.render(
   <React.StrictMode>
     <App />
